ad5c3c5aa2e7319e92002ebe87f1074af114c7e4
afg <afg984@gmail.com>
2015-11-28 19:06:24 +0800

    fix: heroku static background not displayed

--- happycoding/heroku_settings.py
+++ happycoding/heroku_settings.py
@@ -16,7 +16,6 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
