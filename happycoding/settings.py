01e41f64cf16a99caf98241a55f4144bd6ddeef9
afg <afg984@gmail.com>
2015-12-03 09:50:23 +0800

    store secret key in env

--- happycoding/settings.py
+++ happycoding/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$j6*)+m(0xm40v82m0ep-*ld$-_m^ew9pu8l)9v-)=h09$+foo'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'hard to guess string')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
